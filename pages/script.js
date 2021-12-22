bb389f4f9ed658f91eab14fbe5966557497647af
Polinadaskal <91604526+Polinadaskal@users.noreply.github.com>
2021-12-23 09:55:55 +1000

    Add files via upload

---
+++ pages/script.js
@@ -0,0 +1,14 @@
+let block1 = document.createElement('main')
+block1.style.backgroundColor = 'green'
+block1.style.width = '1000px'
+block1.style.height = '200px'
+document.querySelector('body').append(block1)
+
+let block2 = document.createElement('header')
+block2.style.backgroundColor = 'blue'
+block2.style.width = '700px'
+block2.style.height = '200px'
+document.querySelector('body').append(block2)
+let block3 = document.createElement('main')
+document.write(document.img)
+document.querySelector('body').append()
